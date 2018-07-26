15a10d06c6a30663420b1d4edbc3172feb8dc6b6
David MAUGIS <dmaugis@hotmail.com>
2018-07-27 01:31:45 +0200

    improvement

--- zmqnparray.py
+++ zmqnparray.py
@@ -6,24 +6,26 @@ import cv2
 
 def send(socket, array, flags=0, copy=True, track=False,extra=None):
     A = np.ascontiguousarray(array)
-    md = dict(
-         dtype = str(A.dtype),
-         shape = A.shape,
-    )
+    md = {}
     if extra is not None:
        md["extra"]=extra
+    if A is not None:
+       md['dtype'] = str(A.dtype)
+       md['shape'] = A.shape
+       #cv2.imshow('zmqnparray::send',A)
+       #cv2.waitKey(0)
     socket.send_json(md, flags|zmq.SNDMORE)
     return socket.send(A, flags, copy=copy, track=track)
 
 def recv(socket, flags=0, copy=True, track=False):
     extra=None
     md = socket.recv_json(flags=flags)
+    print "md: ",md
     msg = socket.recv(flags=flags, copy=copy, track=track)
     extra=md.pop('extra', None)
     A=None
     if 'dtype' in md:
        if 'shape' in md:
-          print 'md ',md 
           A = np.frombuffer(msg, dtype=md['dtype'])
           A = A.reshape(md['shape'])
           #cv2.imshow('zmqnparray::recv',A)
